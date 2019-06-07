36187052b699996d2c22b453713e06cf81df5573
Stephen Hemminger <stephen@networkplumber.org>
2019-06-07 15:42:14 -0700

    kni: drop unused fields
    The kni net structure only exists in driver no API/ABI.
    Several fields were either totally unused or set and never used.
    
    The fields in dev_info do need to stay in the ABI but
    kernel can ignore them.
    
    Signed-off-by: Stephen Hemminger <stephen@networkplumber.org>
    
    kni: drop unused status element
    
    Yet another ethtool leftover.
    
    Signed-off-by: Stephen Hemminger <stephen@networkplumber.org>

--- kernel/linux/kni/kni_dev.h
+++ kernel/linux/kni/kni_dev.h
@@ -39,8 +39,6 @@ struct kni_dev {
 	/* kni list */
 	struct list_head list;
 
-	int status;
-	uint16_t group_id;           /* Group ID of a group of KNI devices */
 	uint32_t core_id;            /* Core ID to bind */
 	char name[RTE_KNI_NAMESIZE]; /* Network device name */
 	struct task_struct *pthread;
@@ -48,9 +46,6 @@ struct kni_dev {
 	/* wait queue for req/resp */
 	wait_queue_head_t wq;
 	struct mutex sync_lock;
-
-	/* PCI device id */
-	uint16_t device_id;
 
 	/* kni device */
 	struct net_device *net_dev;
@@ -81,9 +76,6 @@ struct kni_dev {
 
 	/* mbuf size */
 	uint32_t mbuf_size;
-
-	/* synchro for request processing */
-	unsigned long synchro;
 
 	/* buffers */
 	void *pa[MBUF_BURST_SZ];
